4efd2aa964f57b1a5f0bb89f43ceab5acb9048f8
Anselmo Blanco Dominguez <anselmo@cuecasduomo.com.br>
2019-01-11 20:23:39 -0200

    change __str__ de curso

--- curso/models.py
+++ curso/models.py
@@ -19,7 +19,7 @@ class Curso(models.Model):
         verbose_name='Nome')
 
     def __str__(self):
-        return self.nome
+        return '({}) {}'.format(self.trilha.nome, self.nome)
 
     class Meta:
         db_table = "c_curso"
