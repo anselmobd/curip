e4b0460b272a489ec16d46e9a770faf20b2a84d9
Anselmo Blanco Dominguez <anselmo@cuecasduomo.com.br>
2019-01-13 11:36:45 -0200

    table curso add carga material mensalidade meses

--- curso/models.py
+++ curso/models.py
@@ -5,7 +5,6 @@ from django.core.validators import MinValueValidator
 class Trilha(models.Model):
     nome = models.CharField(
         max_length=100,
-        verbose_name='Nome',
     )
     meses = models.DecimalField(
         'Duração (meses)',
@@ -35,10 +34,35 @@ class Trilha(models.Model):
 
 class Curso(models.Model):
     trilha = models.ForeignKey(
-        Trilha, on_delete=models.CASCADE)
+        Trilha, on_delete=models.CASCADE,
+    )
     nome = models.CharField(
         max_length=100,
-        verbose_name='Nome')
+    )
+    carga = models.PositiveIntegerField(
+        'carga horária',
+        default=0,
+    )
+    meses = models.DecimalField(
+        'Duração (meses)',
+        max_digits=3,
+        decimal_places=1,
+        default=0,
+        validators=[MinValueValidator(0)]
+    )
+    mensalidade = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        default=0,
+        validators=[MinValueValidator(0)]
+    )
+    material = models.DecimalField(
+        'Material Didádico',
+        max_digits=6,
+        decimal_places=2,
+        default=0,
+        validators=[MinValueValidator(0)]
+    )
 
     def __str__(self):
         return '({}) {}'.format(self.trilha.nome, self.nome)
