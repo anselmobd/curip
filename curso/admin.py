ff68914024ce5186a37523782f6ebe66c70de20a
Anselmo Blanco Dominguez <anselmo@cuecasduomo.com.br>
2019-01-13 11:10:34 -0200

    ordering

--- curso/admin.py
+++ curso/admin.py
@@ -5,9 +5,13 @@ from .models import Trilha, Curso
 
 @admin.register(Trilha)
 class TrilhaAdmin(admin.ModelAdmin):
+    search_fields = ['nome']
+    ordering = ['nome']
     pass
+
 
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
     search_fields = ['trilha__nome', 'nome']
+    ordering = ['trilha__nome', 'nome']
     pass
