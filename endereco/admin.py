de4eb8385dceb4e801fad1288ac7de82ddba3536
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-01-15 16:11:24 -0200

    add js to table admin to do uppercase in a field

--- endereco/admin.py
+++ endereco/admin.py
@@ -1,5 +1,8 @@
+import os
+
 from django.contrib import admin
 
+from curip import settings
 from .models import UF
 
 
@@ -8,3 +11,7 @@ class UfAdmin(admin.ModelAdmin):
     search_fields = ['sigla', 'nome']
     ordering = ['sigla']
     list_display = ['__str__']
+
+    class Media:
+        static_url = getattr(settings, 'STATIC_URL', 'static')
+        js = [os.path.join(static_url, 'admin', 'endereco', 'uf.js'), ]
