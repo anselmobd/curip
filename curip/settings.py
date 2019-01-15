c174fe65c09fe59dc03513ac0c7b27df3153458d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-01-14 22:08:55 -0200

    indicate template folder

--- curip/settings.py
+++ curip/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'curip.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "template"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
