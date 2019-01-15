f53de1a6cbde87a02255c75edb44a991ef25011e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-01-14 22:04:32 -0200

    create test index view

--- curip/urls.py
+++ curip/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import IndexView
+
+
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+
     path('admin/', admin.site.urls),
 ]
